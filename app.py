ef8bb5283b1817a5af8ea54042951695de8c4b2f
seung-doitnow <seung.doitnow@gmail.com>
2023-08-09 22:09:55 +0900

    mode에 맞는 동영상 리스트 불러오는 기능

--- app.py
+++ app.py
@@ -6,14 +6,30 @@ app = Flask(__name__)
 
 ca = certifi.where()
 
-DB_URL=''
+import time
+
+DB_URL='mongodb+srv://seungitnow:tmd123@cluster0.qg1bilm.mongodb.net/?retryWrites=true&w=majority'
 client = MongoClient(DB_URL,tlsCAFile=ca)
-db = client.dbsparta
+db = client.dbseungitnow
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/home', methods=["GET"])
+def return_videos():
+    hour = time.localtime(time.time()).tm_hour
+    if (9 <= hour & hour <= 17):
+        is_working_time = True
+    else:
+        is_working_time = False
+    
+    if (is_working_time):
+        movie_list = list(db.work_videos.find({}, {'_id': False}))
+    else:
+        movie_list = list(db.rest_videos.find({}, {'_id': False}))
+
+    return jsonify({'videos': movie_list})
 
 # 로직 추가
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,23 @@
-alert("hello!")
+$(document).ready(function () {
+    request_movie();
+});
+
+function request_movie() {
+    fetch('/home').then((res) => res.json()).then((data) => {
+        let movies = data['videos']
+
+        movies.forEach((movie) => {
+            let writer = movie['writer'];
+            let thumbnail_url = movie['thumbnail_url'];
+            let video_url = movie['video_url'];
+
+            let movie_card = `<div class="card">
+                                <img src="${thumbnail_url}" class="img-fluid card_image" alt="...">
+                                <div class="card_title">${video_url}</div>
+                                <button class="card_button">보러 가기 -></button>
+                              </div>`
+
+            $('#card_list').append(movie_card);
+        })
+    })
+}
