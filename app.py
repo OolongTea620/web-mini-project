792fa4d7357987509cbecfe6d7c3645e21d4abf7
seung-doitnow <seung.doitnow@gmail.com>
2023-08-10 16:36:21 +0900

    동영상 추가 시 썸네일 및 제목을 추출하는 기능

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-import certifi
+from bs4 import BeautifulSoup
+import certifi, requests, time
 
 app = Flask(__name__)
 
 ca = certifi.where()
-
-import time
 
 DB_URL='mongodb+srv://seungitnow:tmd123@cluster0.qg1bilm.mongodb.net/?retryWrites=true&w=majority'
 client = MongoClient(DB_URL,tlsCAFile=ca)
@@ -17,7 +16,8 @@ def home():
     return render_template('index.html')
 
 @app.route('/home', methods=["GET"])
-def return_videos():
+def return_video_list():
+    # 서버 PC에서 로컬 시각 측정 후 그에 맞는 로직 실행
     hour = time.localtime(time.time()).tm_hour
     if (9 <= hour & hour <= 17):
         is_working_time = True
@@ -25,13 +25,49 @@ def return_videos():
         is_working_time = False
     
     if (is_working_time):
-        movie_list = list(db.work_videos.find({}, {'_id': False}))
+        video_list = list(db.work_videos.find({}, {'_id': False}))
     else:
-        movie_list = list(db.rest_videos.find({}, {'_id': False}))
+        video_list = list(db.rest_videos.find({}, {'_id': False}))
 
-    return jsonify({'videos': movie_list})
+    return jsonify({'res_videoList': video_list})
+
+@app.route('/home', methods=["POST"])
+def add_videos():
+    video_url = request.form['req_url']
+    # 동영상 ID 추출 후 썸네일 주소 추출
+    video_id = search_id(video_url)
+    thumbnail_url = 'https://img.youtube.com/vi/%s/hqdefault.jpg'%video_id
+    # 동영상 제목 크롤링
+    response = requests.get(video_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.select_one('meta[itemprop="name"][content]')['content']
+
+    doc = {
+        'thumbnail_url': thumbnail_url,
+        'title': title
+    }
+
+    mode = request.form['req_mode']
+    if (mode == 'work'):
+        db.work_videos.insert_one(doc)
+    else:
+        db.rest_videos.insert_one(doc)
+
+    return jsonify({'msg': '성공!'})
 
 # 로직 추가
+
+def search_id(url):
+    start_index = url.find('=')
+    last_index = url.find('&')
+    id_from_url = ''
+
+    if last_index == -1:
+        id_from_url = url[start_index + 1:]
+    else:
+        id_from_url = url[start_index + 1:last_index]
+    
+    return id_from_url
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,34 @@
 $(document).ready(function () {
-    request_movie();
+    request_videoList();
 });
 
-function request_movie() {
+function request_videoList() {
     fetch('/home').then((res) => res.json()).then((data) => {
-        let movies = data['videos']
+        let videoList = data['res_videoList']
 
-        movies.forEach((movie) => {
-            let writer = movie['writer'];
-            let thumbnail_url = movie['thumbnail_url'];
-            let video_url = movie['video_url'];
+        videoList.forEach((video) => {
+            let image = video['thumbnail_url']
+            let title = video['title']
 
-            let movie_card = `<div class="card">
-                                <img src="${thumbnail_url}" class="img-fluid card_image" alt="...">
-                                <div class="card_title">${video_url}</div>
-                                <button class="card_button">보러 가기 -></button>
+            let video_card = `<div class="card">
+                                <img src="${image}" class="img-fluid card_image" alt="...">
+                                <div class="card_title">${title}</div>
                               </div>`
 
-            $('#card_list').append(movie_card);
+            $('#card_list').append(video_card);
         })
+    })
+}
+
+document.getElementById("addVideo_btn").onclick = function () {
+    let url = "https://www.youtube.com/watch?v=XSKxJ7BpTyo";
+    let mode = 'work';
+
+    let formData = new FormData();
+    formData.append('req_url', url)
+    formData.append('req_mode', mode)
+
+    fetch('/home', { method: "POST", body: formData }).then((res) => res.json()).then((data) => {
+        alert(data['msg']);
     })
 }
